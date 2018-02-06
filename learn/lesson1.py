cf32c257ba7705d08eebd5232a334f0e35e3096c
agusanaluam <sugaanaluam@gmail.com>
2018-02-06 10:14:23 +0700

    agus maulana

---
+++ learn/lesson1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+file =open("chatlog.txt", "w")
+
+file.write("hello\n")
+file.write("world\n")
+file.write("this my fisrt time learn python")
+
+file.close()
--- sample/echo.py
+++ sample/echo.py
@@ -1,17 +1,30 @@
+#!/usr/bin/python
+
 import time
 from webwhatsapi import WhatsAPIDriver
 from webwhatsapi.objects.message import Message
 
-driver = WhatsAPIDriver()
+
+driver = WhatsAPIDriver(username="maulana")
 print("Waiting for QR")
 driver.wait_for_login()
 
 print("Bot started")
 
 while True:
+
     time.sleep(3)
     print('Checking for more messages')
+    #print driver.get_unread()
     for contact in driver.get_unread():
+
+        #print contact.messages
         for message in contact.messages:
+            #print message.content
             if isinstance(message, Message): # Currently works for text messages only.
-                contact.chat.send_message(message.safe_content)
+                file =open("chatlog.txt", "w")
+                file.write(message.sender)
+                print message.sender
+                print message.content
+                #contact.chat.send_message("Sorry this account is under maintenance, please wait, i'll be back")
+                file.close()
